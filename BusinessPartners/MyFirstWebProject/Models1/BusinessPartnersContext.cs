b656a30d99109599e636f1c3f6af3f9495270691
EsBien <96113739+EsBien@users.noreply.github.com>
2023-07-03 13:01:15 +0300

    clean code

--- BusinessPartners/MyFirstWebProject/Models1/BusinessPartnersContext.cs
+++ BusinessPartners/MyFirstWebProject/Models1/BusinessPartnersContext.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
-namespace MyFirstWebProject.Models;
+namespace MyFirstWebProject.Models1;
 
 public partial class BusinessPartnersContext : DbContext
 {
